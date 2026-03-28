808adc7c18a25d6348b3c7ac93dd0ca093706fa4
Patrick Simpson <patricksimpson.fx@gmail.com>
2026-03-27 22:10:28 -0400

    Remove redundant club name text and revert to standard 4x2 portrait orientation (1.6.3)

--- install-and-run.ps1
+++ install-and-run.ps1
@@ -1,5 +1,5 @@
 # Awana Label Print Server -- All-in-One Installer
-# Version    : 1.6.2
+# Version    : 1.6.3
 # Updated    : 2026-03-27
 #
 # This script:
@@ -24,7 +24,7 @@ param(
 )
 
 $ErrorActionPreference = "Stop"
-$ScriptVersion = "1.6.2"
+$ScriptVersion = "1.6.3"
 
 # Global error handler: pause before exiting on error so user can see what went wrong
 trap {
