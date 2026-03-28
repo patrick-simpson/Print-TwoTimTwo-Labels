932f514a5a44058b87f1251b035c8b88b5f66534
Patrick Simpson <patricksimpson.fx@gmail.com>
2026-03-27 22:07:48 -0400

    Aggressively clear installation directory on update to remove remnants (1.6.1)

--- install-and-run.ps1
+++ install-and-run.ps1
@@ -1,5 +1,5 @@
 # Awana Label Print Server -- All-in-One Installer
-# Version    : 1.6.0
+# Version    : 1.6.1
 # Updated    : 2026-03-27
 #
 # This script:
@@ -24,7 +24,7 @@ param(
 )
 
 $ErrorActionPreference = "Stop"
-$ScriptVersion = "1.6.0"
+$ScriptVersion = "1.6.1"
 
 # Global error handler: pause before exiting on error so user can see what went wrong
 trap {
